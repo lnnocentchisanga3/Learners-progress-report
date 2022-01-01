283dcf88fb15141f7c3ce6e85665f36f2e2134bc
Prosper Muziya <muziyaprosper8@gmail.com>
2022-01-01 04:06:30 -0800

    Update index.php
    modified cetain area

--- template/index.php
+++ template/index.php
@@ -318,100 +318,36 @@
 						</div>
 						<div class="col-sm-6">
 							<div class="d-flex align-items-center justify-content-md-end">
-								<div class="pe-1 mb-3 mb-xl-0">
+								<!-- <div class="pe-1 mb-3 mb-xl-0">
 										<button type="button" class="btn btn-outline-inverse-info btn-icon-text">
 											Feedback
 											<i class="mdi mdi-message-outline btn-icon-append"></i>                          
 										</button>
-								</div>
+								</div> -->
 								<div class="pe-1 mb-3 mb-xl-0">
 										<button type="button" class="btn btn-outline-inverse-info btn-icon-text">
 											Help
 											<i class="mdi mdi-help-circle-outline btn-icon-append"></i>                          
 									</button>
 								</div>
-								<div class="pe-1 mb-3 mb-xl-0">
+								<!-- <div class="pe-1 mb-3 mb-xl-0">
 										<button type="button" class="btn btn-outline-inverse-info btn-icon-text">
 											Print
 											<i class="mdi mdi-printer btn-icon-append"></i>                          
 										</button>
-								</div>
+								</div> -->
 							</div>
 						</div>
 					</div>
-					<div class="row mt-4">
-						<div class="col-lg-8 grid-margin stretch-card">
-							<div class="card">
-								<div class="card-body">
-									<div class="row">
-										<div class="col-lg-4">
-											<h4 class="card-title">Sales Difference</h4>
-											<canvas id="salesDifference"></canvas>
-											<p class="mt-3 mb-4 mb-lg-0">Lorem ipsum dolor sit amet,
-												consectetur adipisicing elit.
-											</p>
-										</div>
-										<div class="col-lg-5">
-											<h4 class="card-title">Best Sellers</h4>
-											<div class="row">
-												<div class="col-sm-4">
-													<ul class="graphl-legend-rectangle">
-														<li><span class="bg-danger"></span>Automotive</li>
-														<li><span class="bg-warning"></span>Books</li>
-														<li><span class="bg-info"></span>Software</li>
-														<li><span class="bg-success"></span>Video games</li>
-													</ul>
-												</div>
-												<div class="col-sm-8 grid-margin">
-													<canvas id="bestSellers"></canvas>
-												</div>
-											</div>
-											<p class="mt-3 mb-4 mb-lg-0">Lorem ipsum dolor sit amet,
-												consectetur adipisicing elit.
-											</p>
-										</div>
-										<div class="col-lg-3">
-											<h4 class="card-title">Social Media Statistics</h4>
-											<div class="row">
-												<div class="col-sm-12">
-													<div class="progress progress-lg grouped mb-2">
-														<div class="progress-bar  bg-danger" role="progressbar" style="width: 40%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-														<div class="progress-bar bg-info" role="progressbar" style="width: 10%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-														<div class="progress-bar bg-warning" role="progressbar" style="width: 20%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-														<div class="progress-bar bg-success" role="progressbar" style="width: 30%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-													</div>
-												</div>
-												<div class="col-sm-12">
-													<ul class="graphl-legend-rectangle">
-														<li><span class="bg-danger"></span>Instagram (15%)</li>
-														<li><span class="bg-warning"></span>Facebook (20%)</li>
-														<li><span class="bg-info"></span>Website (25%)</li>
-														<li><span class="bg-success"></span>Youtube (40%)</li>
-													</ul>
-												</div>
-											</div>
-											<p class="mb-0 mt-2">Lorem ipsum dolor sit amet,
-												consectetur adipisicing elit.
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="col-lg-4 mb-3 mb-lg-0">
-							<div class="card congratulation-bg text-center">
-								<div class="card-body pb-0">
-									<img src="images/dashboard/face29.png" alt="">  
-									<h2 class="mt-3 text-white mb-3 font-weight-bold">Congratulations
-										Johnson
-									</h2>
-									<p>You have done 57.6% more sales today. 
-										Check your new badge in your profile.
-									</p>
-								</div>
-							</div>
-						</div>
-					</div>
+
+
+
+
+
+
+
+
+
 					<div class="row">
 						<div class="col-sm-8 flex-column d-flex stretch-card">
 							<div class="row">
