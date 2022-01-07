da1c3765bc38ace6064a2592deb4adb1c28928a0
Prosper Muziya <muziyaprosper8@gmail.com>
2022-01-06 20:05:25 -0800

    form

--- template/pages/Teacher/my_scores.php
+++ template/pages/Teacher/my_scores.php
@@ -169,30 +169,26 @@
          		<div class="row">
                  <form class="form" method="POST">
                     <div class="form-group">
-                      <label for="input-group-append"><i class="mdi mdi-account-card-details"></i> User Log Id</label>
-                      <input type="text" name="userid" class="form-control"  placeholder="User ID">
+                      <label for="input-group-append"><i class="mdi mdi-account-card-details"></i> Mark Id</label>
+                      <input type="text" name="userid" class="form-control"  placeholder="Mark ID">
                     </div>
                     <div class="form-group">
-                      <label for="input-group-append"><i class="mdi mdi-account-circle"></i>Fullnames</label>
-                      <input type="text" name="names" class="form-control"  placeholder="Fullnames">
+                      <label for="input-group-append"><i class="mdi mdi-account-circle"></i>score</label>
+                      <input type="text" name="names" class="form-control"  placeholder="score">
                     </div>
-                    <div class="form-group">
-                      <label for="exampleInputEmail1"><i class="mdi mdi-phone-classic"></i> Phone</label>
-                      <input type="text" name="phone" class="form-control" id="exampleInputEmail1" placeholder="Phone">
+                    <label for="input-group-append"><i class="mdi mdi-account-circle"></i>Subject</label>
+                      <input type="text" name="names" class="form-control"  placeholder="Subject">
+                      <label for="input-group-append"><i class="mdi mdi-account-circle"></i>Test Number</label>
+                      <input type="text" name="names" class="form-control"  placeholder="Test Number">
+                      <label for="input-group-append"><i class="mdi mdi-account-circle"></i>Term</label>
+                      <input type="text" name="names" class="form-control"  placeholder="Term">
+                      <label for="input-group-append"><i class="mdi mdi-account-circle"></i>Pupil ID</label>
+                      <input type="text" name="names" class="form-control"  placeholder="Pupil ID">
+                      <label for="input-group-append"><i class="mdi mdi-account-circle"></i>Teacher ID</label>
+                      <input type="text" name="names" class="form-control"  placeholder="Teacher ID">
                     </div>
-                    <div class="form-group">
-                      <label for="exampleInputPassword1"><i class="mdi mdi-account-star"></i> User Type</label>
-                      <select class="form-control py-3" name="usertype">
-                        <option>Admin</option>
-                        <option>Teacher</option>
-                      </select>
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputConfirmPassword1"><i class="mdi mdi-account-key"></i> Password</label>
-                      <input type="password" name="pwd" class="form-control" id="exampleInputConfirmPassword1" placeholder="Password">
-                    </div>
-                    <!-- <button type="submit" name="submit_teacher" class="btn btn-primary me-2">Submit <i class="mdi mdi-near-me"></i></button>
-                    <button class="btn btn-danger" type="reset">Cancel <i class="mdi mdi-block-helper"></i></button> -->
+                    <button type="submit" name="submit_teacher" class="btn btn-primary me-2">Submit <i class="mdi mdi-near-me"></i></button>
+                    <button class="btn btn-danger" type="reset">Cancel <i class="mdi mdi-block-helper"></i></button>
                   </form>  
          		</div>
          	</div>
